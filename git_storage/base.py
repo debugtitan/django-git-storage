481f502f87fe8eb5a44de5ce40b3c4d94bf6bde8
Debug Titan <alts.devs@gmail.com>
2023-11-02 21:25:57 +0100

    publish

--- git_storage/base.py
+++ git_storage/base.py
@@ -8,8 +8,17 @@ from django.core.exceptions import ImproperlyConfigured
 from django.dispatch import receiver
 from django.test.signals import setting_changed
 
-GIT_STORAGE_CONFIG = getattr(settings, "GIT_STORAGE", {})
-GIT_ACCESS_TOKEN, GIT_REPO = itemgetter("GIT_ACCESS_TOKEN", "GIT_REPO")(GIT_STORAGE_CONFIG)
+settings.configure()
+try:
+    GIT_STORAGE_CONFIG = getattr(settings, "GIT_STORAGE", {})
+    GIT_ACCESS_TOKEN, GIT_REPO = itemgetter("GIT_ACCESS_TOKEN", "GIT_REPO")(GIT_STORAGE_CONFIG)
+except KeyError:
+    raise ImproperlyConfigured(
+            "In order to use Github storage, you need to provide "
+            "'GIT_STORAGE' dictionary with 'GIT_ACCESS_TOKEN' "
+            "and 'GIT_REPO' in the settings (or 'GITHUB_ACCESS_TOKEN', 'GITHUB_REPO' "
+            "variables)."
+        )
 
 if not GIT_ACCESS_TOKEN or not GIT_REPO:
     if not (os.environ.get("GITHUB_ACCESS_TOKEN") and os.environ.get("GITHUB_REPO")):
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+description = (
+    'Django Github Storage Package '
+)
+
+setup(
+    name='django-git-storages',
+    version='0.0.1',
+    author='Uche David',
+    author_email='debugtitan.hub@outlook.com',
+    description=description,
+    long_description="Django package that facilitates integration with Github as it's storage provider",
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/debugtitan/django-git-storage',
+    keywords=['django git storage','django github storage'],
+    packages=['git_storage'],
+    include_package_data=True,
+    classifiers = [
+        "Framework :: Django",
+        "Programming Language :: Python :: 3",
+    ]
+)
